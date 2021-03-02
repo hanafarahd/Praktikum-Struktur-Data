5cda2328ace86fcd37e8f4c71918aac578d1dead
hanafarahd <72843256+hanafarahd@users.noreply.github.com>
2021-03-02 21:53:56 +0700

    Add files via upload

---
+++ prak.2_array B1.cpp
@@ -0,0 +1,25 @@
+// Nama: Hana Farahdiana
+// NIM: 20051397073
+// Kelas: 2020A
+// (1) ARRAY - Program Membalik Huruf 'P','E', 'N', 'S', 'I', 'T'
+
+#include <iostream>
+using namespace std;
+
+int main(){
+	cout << "PROGRAM ARRAY MEMBALIK HURUF" << endl;
+	cout << "============================" << endl;
+	char a[6] = {'p', 'e', 'n', 's', 'i', 't'};
+	int i, j;
+	
+	cout << "--Kata sebelum dibalik : --"<< endl;
+	for (i=0; i<6; i++){
+		cout << a [i];	
+	}
+	cout << "\n\n";
+	cout << "--kata setelah dibalik : --"<< endl;
+	for (j=6; j>=0; j--){
+		cout << a [j];
+	}
+	return 0;
+}
