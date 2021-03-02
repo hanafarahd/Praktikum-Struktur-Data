cfc26d7ce0f47227ffd1c3a3aa9588cc61c264dc
hanafarahd <72843256+hanafarahd@users.noreply.github.com>
2021-03-02 21:59:10 +0700

    Add files via upload

---
+++ prak.2_ stack C4.cpp
@@ -0,0 +1,35 @@
+// Nama: Hana Farahdiana
+// NIM: 20051397073
+// Kelas: 2020A
+// (4) STACK - program pengecekan kata palindrome -
+
+#include<iostream>
+#include<string.h>
+
+using namespace std;
+
+int main(){
+	char kata[40];
+	int i, n;
+	
+	cout << "===========================================" << endl;
+    cout << "\t\t STACK \t" << endl;
+    cout << "-PROGRAM MENGECEK KATA PALINDROM ATAU BUKAN-" << endl;
+    cout << "=========================================== \n" << endl;
+    cout << "masukkan kata : ";
+    cin >> kata;
+    
+    n = strlen(kata);
+    for (i=0; i<n; i++){
+
+    if (kata[i] != kata[n-1-i]){
+    i = n;
+    cout << "Kata tersebut bukan palindrom" << endl;
+        
+    } else {
+    i = n;
+    cout << "Kata tersebut adalah palindrom" << endl;
+
+       }
+    } 
+}
